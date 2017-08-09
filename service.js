09769899431683ea716c4dce3f3ac7589e70d2d5
Spencer Henze <spencerhenze@Spencers-iMac.local>
2017-08-09 00:12:30 -0600

    https resolve attempt

--- service.js
+++ service.js
@@ -14,8 +14,8 @@ function NFLService(url, callback) {
             //this will prevent the code below from ever executing
         }
 
-        var url = "http://bcw-getter.herokuapp.com/?url=";
-        var endpointUri = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
+        var url = "https://bcw-getter.herokuapp.com/?url=";
+        var endpointUri = "https://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
         var apiUrl = url + encodeURIComponent(endpointUri);
 
         $.getJSON(apiUrl, function (data) {
@@ -111,7 +111,6 @@ function NFLService(url, callback) {
 
     // call the loadPlayersData() function every time we create a new service
     loadPlayersData();
-    console.log(playersData);
 
 
 
